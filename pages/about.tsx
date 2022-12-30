af020cbfa98d74016642788c7cc1435c989b7c86
malich.d <malich.d@citilink.ru>
2022-12-30 23:55:40 +0300

    added

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import {FC} from "react";
+import {Head} from "next/document";
+
+export default function About(){
+    return(
+        <>
+            <div>THIS IS NEXT</div>
+        </>
+    )
+}
+
